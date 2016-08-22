f0bca79bb7c17cba50b1132849a2991907f07a86
Nikita Golovin <golovin.nik@gmail.com>
2016-08-22 03:16:08 +0300

    gravatar to user

--- src/components/sidebar/User/User.js
+++ src/components/sidebar/User/User.js
@@ -1,24 +1,21 @@
 import React, {Component, PropTypes} from 'react';
 import CSSModules from 'react-css-modules';
 import InlineSVG from 'svg-inline-react';
+import Gravatar from 'react-gravatar';
 
 import styles from './User.sss';
 
 
 @CSSModules(styles, {allowMultiple: true})
 export default class User extends Component {
-  componentDidMount() {
-  }
-
   render() {
     const {userData} = this.props;
-    const imgUrl = 'https://s3.amazonaws.com/uifaces/faces/twitter/zeldman/128.jpg'; /// it needs to receive url from gravatar
 
     return (
       <div styleName="user">
         <div styleName="profile">
           <div styleName="avatar">
-            <div styleName="avatar-image" style={{backgroundImage: 'url(' + imgUrl + ')'}}></div>
+            <Gravatar email={userData.email} styleName="gravatar"/>
           </div>
           <div styleName="avatar-name">
             {userData.firstName} {userData.lastName}
