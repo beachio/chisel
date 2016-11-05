a56b2bb0d57aefe8a5c4e98e7aae47783888b755
Nikita Golovin <golovin.nik@gmail.com>
2016-11-05 15:59:09 +0300

    content — title correction

--- src/models/ContentData.js
+++ src/models/ContentData.js
@@ -10,15 +10,20 @@ export class ContentItemData {
   fields = new Map();
   
   //setter
+  _titleField = null;
   _title = "";
   _slug = "";
   
   //links
-  model = null;
+  _model = null;
+  
+  
+  get titleField() {return this._titleField;}
   
   get title() {return this._title;}
   set title(title) {
     this._title = removeSpaces(title);
+    this.fields.set(this._titleField, this._title);
     this.slug = this._title;
   }
   
@@ -27,20 +32,37 @@ export class ContentItemData {
     this._slug = filterSpecials(slug);
   }
   
+  get model() {return this._model;}
+  set model(model) {
+    this._model = model;
+    
+    this.fields = new Map();
+    for (let field of this.model.fields) {
+      this.fields.set(field, null);
+      
+      if (field.isTitle)
+        this._titleField = field;
+    }
+  }
+  
   get OriginClass() {
-    return Parse.Object.extend(this.model.tableName);
+    if (this.model)
+      return Parse.Object.extend(this.model.tableName);
+    return null;
   }
   
   setOrigin(origin) {
     this.origin = origin;
     
-    if (origin.get('title'))  this.title  = origin.get('title');
     if (origin.get('slug'))   this.slug   = origin.get('slug');
     if (origin.get('color'))  this.color  = origin.get('color');
     
     for (let field of this.model.fields) {
       let value = origin.get(field.nameId);
       this.fields.set(field, value);
+  
+      if (field.isTitle)
+        this._title = value;
     }
     
     return this;
@@ -50,12 +72,11 @@ export class ContentItemData {
     if (!this.origin)
       this.origin = new this.OriginClass;
     
-    this.origin.set("title",  this.title);
     this.origin.set("slug",   this.slug);
     this.origin.set("color",  this.color);
   
     for (let field of this.model.fields) {
-      if (field.nameId == "title" || field.nameId == "color" || field.nameId == "slug")
+      if (field.nameId == "color" || field.nameId == "slug")
         continue;
       let value = this.fields.get(value);
       this.origin.set(field.nameId, value);
