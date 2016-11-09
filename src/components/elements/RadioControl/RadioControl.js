d566cedccd3a73db42bf9f9a4ffd1669e033612c
Tigran Tumasov <shugar348@gmail.com>
2016-11-09 21:42:16 +0300

    radiocontrol not completed

---
+++ src/components/elements/RadioControl/RadioControl.js
@@ -0,0 +1,36 @@
+import React, {Component, PropTypes} from 'react';
+import CSSModules from 'react-css-modules';
+import InlineSVG from 'svg-inline-react';
+
+import styles from './DropdownControl.sss';
+
+
+@CSSModules(styles, {allowMultiple: true})
+export default class DropdownControl extends Component {
+
+  render() {
+    const { value, onRadioChange } = this.props;
+
+    let RadioButtons = this.radioButtonsList.map(radioButton => {
+      return (
+        <div styleName="radio-button">
+          <input styleName="radio"
+                 type="radio"
+                 id={ /* id инпута */ }
+                 name="radio"
+                 checked={ /* value инпута */ }
+                 onChange={ /* onChange инпута */ } />
+          <label styleName="radio-label" htmlFor={ /* id инпута */ }>
+            { /* здесь должен быть текст лейбла */ }
+          </label>
+        </div>
+      )
+    });
+
+    return (
+      <div styleName="RadioControl">
+        {RadioButtons}
+      </div>
+    );
+  }
+}
