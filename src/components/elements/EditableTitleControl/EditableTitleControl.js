c20b87a5d5110c3f38328ca21ebd1889e3366128
Tigran Tumasov <shugar348@gmail.com>
2016-11-10 20:41:46 +0300

    edit button fixed ?

--- src/components/elements/EditableTitleControl/EditableTitleControl.js
+++ src/components/elements/EditableTitleControl/EditableTitleControl.js
@@ -4,7 +4,7 @@ import CSSModules from 'react-css-modules';
 import styles from './EditableTitleControl.sss';
 
 
-const MIN_TEXT = '1234567890';
+const MIN_TEXT = '12345';
 
 @CSSModules(styles, {allowMultiple: true})
 export default class EditableTitleControl extends Component {
@@ -14,25 +14,25 @@ export default class EditableTitleControl extends Component {
     width: 0
   };
   testTextElm = null;
-  
-  
+
+
   setText(text) {
     let wText = text;
     if (!wText)
       wText = this.props.placeholder;
     if (!wText || wText.length < MIN_TEXT.length)
       wText = MIN_TEXT;
-    
+
     this.testTextElm.innerText = wText;
     let width = this.testTextElm.clientWidth * 1.1;
-    
+
     this.setState({text, width});
   }
-  
+
   componentWillReceiveProps(nextProps) {
     this.setText(nextProps.text);
   }
-  
+
   componentDidMount() {
     this.testTextElm = document.createElement('div');
     this.testTextElm.style.fontSize = this.props.isSmall ? '12px' : '20px';
@@ -42,30 +42,30 @@ export default class EditableTitleControl extends Component {
     this.testTextElm.style.left = '0';
     this.testTextElm.style.zIndex = '-1';
     document.body.appendChild(this.testTextElm);
-  
+
     this.setText(this.props.text);
   }
-    
+
   onEditClick = () => {
     this.setState(
       {editing: true},
       () => this.refs.input.focus()
     );
   };
-  
+
   onChange = event => {
     this.setText(event.target.value);
   };
-  
+
   onBlur = () => {
     this.setState({editing: false});
     this.props.cancel();
   };
-  
+
   onKeyDown = event => {
     if (this.props.alertShowing)
       return;
-    
+
     //Enter pressed
     if (event.keyCode == 13) {
       this.props.update(this.title);
@@ -74,16 +74,16 @@ export default class EditableTitleControl extends Component {
       this.props.cancel();
     }
   };
-  
+
   render() {
     const {placeholder, isSmall} = this.props;
-  
+
     let styleName = "input";
     if (this.state.editing)
       styleName += " input-editing";
     if (isSmall)
       styleName += " input-small";
-    
+
     return (
       <div styleName="wrapper">
         <input style={{width: this.state.width + 'px'}}
