145883e65c07902030efbbbf553789c376e99d63
Nikita Golovin <golovin.nik@gmail.com>
2017-04-29 19:34:26 +0300

    added SashiDo config

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,7 @@
-//const SERVER_URL = "https://parse.nuwe.co:49178/parse";
 export const SERVER_URL = "http://localhost:1337/parse";
+//export const SERVER_URL = "https://pg-app-89ruw0d0g8ew95dp7dy7sgeqvbp7f6.scalabl.cloud/1/";
+
 export const APP_ID = "d5701a37cf242d5ee398005d997e4229";
+//export const APP_ID = "R6T4Eal4vaNrw6RnwSLvEWtNMmrRPjLiS3FRG4MI";
 
 export const FILE_SIZE_MAX = 10 * 1024 * 1024;
