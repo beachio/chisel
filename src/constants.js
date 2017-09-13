9a90c38069060ac54912aee26a8d4c9b0ab08d42
Никита Головин <golovin.nik@gmail.com>
2017-09-14 00:06:27 +0300

    connecting to Heroku by default

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,7 @@
 export let config = {
-  serverURL: "http://localhost:1337/parse",
+  //serverURL: "http://localhost:1337/parse",
   //serverURL: "https://pg-app-mjx6kfptatphej7zd7ccy9ctlkdtix.scalabl.cloud/1/",
+  serverURL: "https://chisel-parse-server.herokuapp.com/parse",
   appId: "d5701a37cf242d5ee398005d997e4229",
   //appId: "tUZa9hFloenXnR1pnwAW8644VJTRWUOjjPMJPH3n",
   JSkey: `liYLwLfENUIiiD6bz8TerwIZPPnJWP3VVHCSUUOT`,
