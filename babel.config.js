cdfb6e3d4696c05a385d0f864e822f38ba5d19b5
Nes-si <golovin.nik@gmail.com>
2018-10-12 08:41:04 +0300

    Updating dependencies. Updating to Babel v7.

---
+++ babel.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        modules: false,
+        useBuiltIns: "usage"
+      }
+    ],
+    "@babel/preset-react"
+  ],
+
+  plugins: [
+    "@babel/plugin-syntax-dynamic-import",
+    "@babel/plugin-syntax-import-meta",
+    "@babel/plugin-proposal-class-properties",
+
+    ["@babel/plugin-proposal-decorators", {legacy: true}],
+    "@babel/plugin-proposal-export-namespace-from",
+    "@babel/plugin-proposal-numeric-separator",
+    "@babel/plugin-proposal-throw-expressions",
+
+    //"@babel/plugin-transform-runtime",
+    //"@babel/plugin-transform-regenerator",
+
+    "babel-plugin-lodash",
+    "react-hot-loader/babel"
+  ]
+};
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import 'babel-polyfill';
 import 'whatwg-fetch';
 
 import 'normalize.css';
