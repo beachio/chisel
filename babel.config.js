5d5f1ea7fbfac9a221efc0f10021d47193fd78bf
Nes-si <golovin.nik@gmail.com>
2018-10-12 19:29:15 +0300

    Babel decorators fix.

--- babel.config.js
+++ babel.config.js
@@ -13,9 +13,10 @@ module.exports = {
   plugins: [
     "@babel/plugin-syntax-dynamic-import",
     "@babel/plugin-syntax-import-meta",
-    "@babel/plugin-proposal-class-properties",
 
     ["@babel/plugin-proposal-decorators", {legacy: true}],
+    ["@babel/plugin-proposal-class-properties", {loose: true}],
+
     "@babel/plugin-proposal-export-namespace-from",
     "@babel/plugin-proposal-numeric-separator",
     "@babel/plugin-proposal-throw-expressions",
