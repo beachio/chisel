bfba24277ed897a16234101b94a1a7c2bb961092
Nes-si <golovin.nik@gmail.com>
2018-10-23 21:40:41 +0300

    Adding checkbox control

---
+++ src/components/elements/CheckboxControl/CheckboxControl.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import CSSModules from 'react-css-modules';
+import _ from 'lodash/core';
+
+import styles from './CheckboxControl.sss';
+
+
+@CSSModules(styles, {allowMultiple: true})
+export default class CheckboxControl extends Component {
+  id = '0';
+  
+  constructor(props) {
+    super(props);
+    
+    this.id = _.uniqueId('switch_');
+  }
+  
+  onChange = e => {
+    const {onChange, disabled} = this.props;
+    if (onChange && !disabled)
+      onChange(e.target.checked);
+  };
+  
+  render() {
+    const {title, checked, disabled} = this.props;
+    let style = `CheckboxControl`;
+    if (disabled)
+      style += ` disabled`;
+    
+    return (
+      <div styleName={style}>
+        <div styleName="checkbox-button">
+          <input styleName="checkbox"
+                 type="checkbox"
+                 id={this.id}
+                 name="checkbox"
+                 disabled={disabled}
+                 checked={checked}
+                 onChange={this.onChange} />
+          <label styleName="checkbox-label" htmlFor={this.id}>{title}</label>
+        </div>
+      </div>
+    );
+  }
+}
