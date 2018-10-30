e8f0e85300b5ac0cf18a1444be6756698b46b47f
Nes-si <golovin.nik@gmail.com>
2018-10-30 22:00:31 +0300

    RadioControl fixed.

--- src/components/elements/RadioControl/RadioControl.js
+++ src/components/elements/RadioControl/RadioControl.js
@@ -1,35 +1,50 @@
 import React, {Component} from 'react';
 import CSSModules from 'react-css-modules';
-import InlineSVG from 'svg-inline-react';
+import _ from "lodash/core";
 
 import styles from './RadioControl.sss';
 
 
 @CSSModules(styles, {allowMultiple: true})
 export default class DropdownControl extends Component {
+  state = {
+    value: false
+  };
+  id = '0';
+  
+  constructor(props) {
+    super(props);
+    
+    this.state.value = props.value;
+    this.id = _.uniqueId('radio_');
+  }
+  
+  componentWillReceiveProps (nextProps) {
+    this.setState({value: nextProps.value});
+  }
+  
+  onChange = e => {
+    const {onChange, disabled, data} = this.props;
+    if (onChange && !disabled && e.target.checked)
+      onChange(data);
+  };
 
   render() {
-    const { value, onRadioChange } = this.props;
-
-    let RadioButtons = this.radioButtonsList.map(radioButton => {
-      return (
-        <div styleName="radio-button">
-          <input styleName="radio"
-                 type="radio"
-                 id={ /* id инпута */ }
-                 name="radio"
-                 checked={ /* value инпута */ }
-                 onChange={ /* onChange инпута */ } />
-          <label styleName="radio-label" htmlFor={ /* id инпута */ }>
-            { /* здесь должен быть текст лейбла */ }
-          </label>
-        </div>
-      )
-    });
+    const {data, name, label, disabled} = this.props;
+    let style = `RadioControl`;
+    if (disabled)
+      style += ` disabled`;
 
     return (
-      <div styleName="RadioControl">
-        {RadioButtons}
+      <div styleName={style}>
+        <input styleName="input"
+               type="radio"
+               id={this.id}
+               name={name}
+               value={data}
+               checked={this.state.value === data}
+               onChange={this.onChange} />
+        <label styleName="label" htmlFor={this.id}>{label}</label>
       </div>
     );
   }
