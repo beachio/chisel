e4975cf045fe7d2420100aa51da7c3a8233dc493
Nes-si <golovin.nik@gmail.com>
2018-11-06 22:40:34 +0300

    InputNumberControl. Min and max values.

--- src/components/elements/InputNumberControl/InputNumberControl.js
+++ src/components/elements/InputNumberControl/InputNumberControl.js
@@ -7,32 +7,44 @@ export default class InputNumberControl extends Component {
   state = {value: '0'};
   valueParsed = 0;
   parseFunc;
+  min;
+  max;
   
   
   constructor(props) {
     super(props);
   
     this.parseFunc = props.isInt ? parseInt : parseFloat;
+    this.min = props.min;
+    this.max = props.max;
     
     this.state.value = props.value;
     this.valueParsed = this.parseValue(props.value);
   }
   
   componentWillReceiveProps(nextProps) {
-    const {value} = nextProps;
+    const {value, min, max} = nextProps;
+  
+    this.min = min;
+    this.max = max;
     
     const valueParsed = this.parseValue(value);
     if (valueParsed === this.valueParsed)
       return;
     this.valueParsed = valueParsed;
-    
-    this.setState({value});
+    this.setState({value: valueParsed});
   }
   
   parseValue(value) {
-    const num = this.parseFunc(value);
+    let num = this.parseFunc(value);
     if (isNaN(num))
       return undefined;
+  
+    if (this.min !== undefined && this.min !== null && num < this.min)
+      num = this.min;
+    if (this.max !== undefined && this.max !== null && num > this.max)
+      num = this.max;
+    
     return num;
   }
   
