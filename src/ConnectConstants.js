c8eac6de3ee6d44dc51dffe9086cecaa63d35a3e
Nes-si <golovin.nik@gmail.com>
2019-01-11 12:53:45 +0300

    Connect constants — reordering

--- src/ConnectConstants.js
+++ src/ConnectConstants.js
@@ -1,19 +1,22 @@
 export let config = {
-  // serverURL: "http://localhost:1337/parse",
-  //serverURL: "https://pg-app-mjx6kfptatphej7zd7ccy9ctlkdtix.scalabl.cloud/1/",
-  
-  //appId: "d5701a37cf242d5ee398005d997e4229",
-  // appId: "7d52447901ab6b8b91e3f2e846256184",
-  JSkey: `liYLwLfENUIiiD6bz8TerwIZPPnJWP3VVHCSUUOT`,
-  RESTkey: `AMMaWJMu4u6hSANZfbBFZHLhU83DWOXHXPVnPHJE`,
+  // Dev local env
+  serverURL: "http://localhost:1337/parse",
+  appId: 'SampleAppId',
 
-  // Dev env
+  // Dev remote env
   // serverURL: "https://chisel-parse-server.herokuapp.com/parse",
   // appId: 'SampleAppId',
 
+  // Dev remote old env
+  // serverURL: "https://pg-app-mjx6kfptatphej7zd7ccy9ctlkdtix.scalabl.cloud/1/",
+  // appId: "7d52447901ab6b8b91e3f2e846256184",
+
   // Prod env
-  serverURL: "http://dockerhost.forge-parse-server.c66.me:99/parse",
-  appId: "7d52447901ab6b8b91e3f2e846256184",
+  // serverURL: "http://dockerhost.forge-parse-server.c66.me:99/parse",
+  // appId: "7d52447901ab6b8b91e3f2e846256184",
+
+  JSkey: `liYLwLfENUIiiD6bz8TerwIZPPnJWP3VVHCSUUOT`,
+  RESTkey: `AMMaWJMu4u6hSANZfbBFZHLhU83DWOXHXPVnPHJE`
 };
 
 export const FILE_SIZE_MAX = 10 * 1024 * 1024;
