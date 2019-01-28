3219058601706dd7cdbc2f6c3854010f409f611f
Nes-si <golovin.nik@gmail.com>
2019-01-28 06:49:09 +0300

    Sites — simplification.

--- src/components/sidebar/Sites/Sites.js
+++ src/components/sidebar/Sites/Sites.js
@@ -63,47 +63,41 @@ export default class Sites extends Component {
           }
         </div>
         <div styleName="list">
-          {
-            sites.map(site => {
-              let style = "element";
-              if (this.state.site == site)
-                style += " element-active";
+          {sites.map((site, key) => {
+            let style = "element";
+            if (this.state.site == site)
+              style += " element-active";
 
-              const name = site.name;
-              const key = sites.indexOf(site);
+            return (
+              <div styleName={style}
+                   onClick={() => this.onClickSite(site)}
+                   key={key}>
 
-              return (
-                <div styleName={style}
-                     onClick={() => this.onClickSite(site)}
+                {!!site.icon ?
+                  <img styleName="icon-img"
+                       src={site.icon.url()}>
+                  </img>
+                :
+                  <div styleName="icon">
+                     <InlineSVG src={require("assets/images/hammer.svg")}/>
+                  </div>
+                }
 
-                     key={key}>
-
-                  {!!site.icon ?
-                    <img styleName="icon-img"
-                         src={site.icon.url()}>
-                    </img>
-                  :
-                    <div styleName="icon">
-                       <InlineSVG src={require("assets/images/hammer.svg")}/>
-                    </div>
-                  }
-
-                  <div styleName="site-name">{name}</div>
-                  {!!site.domain ?
-                    <a href={site.domain}
-                       target="_blank"
-                       styleName="link">
-                      <InlineSVG src={require("assets/images/link.svg")}/>
-                    </a>
-                  :
-                    <div styleName="link-disabled">
-                      <InlineSVG src={require("assets/images/link.svg")}/>
-                    </div>
-                  }
-                </div>
-              );
-            })
-          }
+                <div styleName="site-name">{site.name}</div>
+                {!!site.domain ?
+                  <a href={site.domain}
+                     target="_blank"
+                     styleName="link">
+                    <InlineSVG src={require("assets/images/link.svg")}/>
+                  </a>
+                :
+                  <div styleName="link-disabled">
+                    <InlineSVG src={require("assets/images/link.svg")}/>
+                  </div>
+                }
+              </div>
+            );
+          })}
         </div>
         <div styleName="new-site" onClick={this.onClickAdd}>
           Create New Site
