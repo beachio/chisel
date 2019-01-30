5e3393bbe0b8843f7253612530614e7f688a3f9f
Nes-si <golovin.nik@gmail.com>
2019-01-30 17:32:50 +0300

    Debounce and throttle functions.

--- src/utils/common.js
+++ src/utils/common.js
@@ -243,3 +243,50 @@ export function setTimeout64(func, delay) {
   else
     setTimeout(func, delay);
 }
+
+
+export function debounce(func, threshold, execAsap) {
+  let timeout;
+
+  return function debounced () {
+    const obj = this, args = arguments;
+    const delayed = () => {
+      if (!execAsap)
+        func.apply(obj, args);
+      timeout = null;
+    };
+
+    if (timeout)
+      clearTimeout(timeout);
+    else if (execAsap)
+      func.apply(obj, args);
+
+    timeout = setTimeout(delayed, threshold || 100);
+  };
+}
+
+export function throttle(func, ms) {
+  let isThrottled = false,
+    savedArgs,
+    savedThis;
+
+  return function wrapper() {
+    if (isThrottled) { // (2)
+      savedArgs = arguments;
+      savedThis = this;
+      return;
+    }
+
+    func.apply(this, arguments); // (1)
+
+    isThrottled = true;
+
+    setTimeout(() => {
+      isThrottled = false; // (3)
+      if (savedArgs) {
+        wrapper.apply(savedThis, savedArgs);
+        savedArgs = savedThis = null;
+      }
+    }, ms);
+  };
+}
