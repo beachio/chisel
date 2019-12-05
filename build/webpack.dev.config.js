b54c0190edeb8cbf0418d57603510b393298711c
Nes-si <golovin.nik@gmail.com>
2019-12-05 07:00:42 +0300

    Build. importLoaders.

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -19,7 +19,12 @@ module.exports = merge(baseWebpackConfig, {
         test: /\.css$|\.global\.sss$/,
         use: [
           'style-loader',
-          'css-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
           'postcss-loader'
         ]
       },
--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -29,7 +29,12 @@ module.exports = merge(baseWebpackConfig, {
             loader: MiniCssExtractPlugin.loader,
             options: {},
           },
-          'css-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
           'postcss-loader'
         ]
       },
