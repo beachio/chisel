b861de4191717ea0f9f2fafec45daa42ace71704
Nes-si <golovin.nik@gmail.com>
2019-12-07 12:34:08 +0300

    Small changes.

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -1,4 +1,3 @@
-const webpack = require('webpack');
 const merge = require('webpack-merge');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 //const StatsPlugin = require('stats-webpack-plugin');
