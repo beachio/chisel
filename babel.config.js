21bc603aae1ec81310638b8c5e0834af3aba59ec
Nes-si <golovin.nik@gmail.com>
2019-12-12 04:09:39 +0300

    Build. Adding @babel/plugin-transform-react-jsx-source.

--- babel.config.js
+++ babel.config.js
@@ -1,32 +1,41 @@
-module.exports = {
-  presets: [
-    [
-      "@babel/preset-env",
-      {
-        modules: false,
-        useBuiltIns: "usage",
-        corejs: {
-          version: '3.3',
-          proposals: true
+module.exports = ({cache, env}) => {
+  cache.using(() => process.env.NODE_ENV);
+
+  const res = {
+    presets: [
+      [
+        "@babel/preset-env",
+        {
+          modules: false,
+          useBuiltIns: "usage",
+          corejs: {
+            version: '3.3',
+            proposals: true
+          }
         }
-      }
+      ],
+      "@babel/preset-react"
     ],
-    "@babel/preset-react"
-  ],
 
-  plugins: [
-    "@babel/plugin-syntax-dynamic-import",
-    "@babel/plugin-syntax-import-meta",
+    plugins: [
+      "@babel/plugin-syntax-dynamic-import",
+      "@babel/plugin-syntax-import-meta",
 
-    ["@babel/plugin-proposal-decorators", {legacy: true}],
-    ["@babel/plugin-proposal-class-properties", {loose: true}],
+      ["@babel/plugin-proposal-decorators", {legacy: true}],
+      ["@babel/plugin-proposal-class-properties", {loose: true}],
 
-    "@babel/plugin-proposal-export-namespace-from",
-    "@babel/plugin-proposal-numeric-separator",
-    "@babel/plugin-proposal-throw-expressions",
+      "@babel/plugin-proposal-export-namespace-from",
+      "@babel/plugin-proposal-numeric-separator",
+      "@babel/plugin-proposal-throw-expressions",
 
-    ["@babel/plugin-transform-runtime", {useESModules: true}],
+      ["@babel/plugin-transform-runtime", {useESModules: true}],
 
-    "react-hot-loader/babel"
-  ]
+      "react-hot-loader/babel"
+    ]
+  };
+
+  if (!env("production"))
+    res.plugins.push('@babel/plugin-transform-react-jsx-source');
+
+  return res;
 };
