e31ecb5960bfabd2b2ba2ebb0c955ff651cdba0d
Nes-si <golovin.nik@gmail.com>
2020-01-07 21:54:49 +0300

    Build. Correct PostCSS plugins order.

--- postcss.config.js
+++ postcss.config.js
@@ -1,24 +1,21 @@
 module.exports = ({file, options, env}) => {
   const res = {
-    plugins: [
-      require('postcss-flexibility'),
-      require('postcss-flexbugs-fixes'),
-      require('autoprefixer')
-    ]
+    plugins: []
   };
-
-  if (env == 'production')
-    res.plugins.unshift(require('cssnano')({
-      preset: ['default', {
-        mergeRules: false
-      }]
-    }));
-
 
   if (file.extname === '.sss') {
     res.parser = 'sugarss';
     res.plugins.push(require('precss'));
   }
+
+  res.plugins.push(
+    require('postcss-flexibility'),
+    require('postcss-flexbugs-fixes'),
+    require('autoprefixer')
+  );
+
+  if (env == 'production')
+    res.plugins.push(require('cssnano'));
 
   return res;
 };
