820f315f7f0d7de4dd2cb5923d201dc315af0f2f
Nikita Golovin <golovin.nik@gmail.com>
2021-02-28 08:18:29 +0300

    Live queries. Field deletion alert.

--- src/middleware/liveUpdates.js
+++ src/middleware/liveUpdates.js
@@ -1,8 +1,8 @@
 import {browserHistory} from "react-router";
 
-import {MODEL_ADD, MODEL_DELETE, SITE_DELETE} from "ducks/models";
+import {FIELD_DELETE, MODEL_ADD, MODEL_DELETE, SITE_DELETE} from "ducks/models";
 import {subscribeToContentItem} from "ducks/content";
-import {showAlert, URL_SITE, URL_USERSPACE} from "ducks/nav";
+import {closeModal, showAlert, URL_SITE, URL_USERSPACE, MODAL_TYPE_FIELD} from "ducks/nav";
 import {ALERT_TYPE_ALERT} from "components/modals/AlertModal/AlertModal";
 
 
@@ -19,9 +19,9 @@ export const controlRemoving = store => next => action => {
       action.fromServer &&
       store.getState().models.currentSite == action.site) {
     next(showAlert({
-      title: `Site <strong>${action.site.name}</strong> was deleted`,
+      title: `The site <strong>${action.site.name}</strong> was deleted`,
       type: ALERT_TYPE_ALERT,
-      description: "Someone just deleted the current site. Press OK to return to next site.",
+      description: "Someone just deleted the current site. Press OK to go to another site.",
       callback: () => next(action)
     }));
   } else {
@@ -31,12 +31,26 @@ export const controlRemoving = store => next => action => {
   if (action.type == MODEL_DELETE &&
       store.getState().models.currentModel == action.model) {
     next(showAlert({
-      title: `Model <strong>${action.model.name}</strong> was deleted`,
+      title: `The model <strong>${action.model.name}</strong> was deleted`,
       type: ALERT_TYPE_ALERT,
       description: "Someone just deleted the current model. Press OK to return to models' list.",
       callback: () => {
         browserHistory.push(`/${URL_USERSPACE}/${URL_SITE}${action.model.site.nameId}`);
       }
     }));
+  }
+
+  if (action.type == FIELD_DELETE) {
+    const {nav} = store.getState();
+    if (nav.modalShowing &&
+        nav.modalType == MODAL_TYPE_FIELD &&
+        nav.modalParams == action.field) {
+      next(closeModal());
+      next(showAlert({
+        title: `The field <strong>${action.field.name}</strong> was deleted`,
+        type: ALERT_TYPE_ALERT,
+        description: "Someone just deleted the field you're edited. Press OK to return to field's list."
+      }));
+    }
   }
 };
