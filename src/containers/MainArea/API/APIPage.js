ce7ef2db2c92066b47c84a0a7085a9792f3d1559
Nikita Golovin <golovin.nik@gmail.com>
2022-10-03 23:06:15 +0400

    Transforming some React classes to stateless functions.

--- src/containers/MainArea/API/APIPage.js
+++ src/containers/MainArea/API/APIPage.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import InlineSVG from 'svg-inline-react';
 import {connect} from 'react-redux';
 import {Helmet} from "react-helmet-async";
@@ -9,43 +9,37 @@ import NoRights from "components/mainArea/common/NoRights";
 import ImageHammer from 'assets/images/hammer.svg';
 
 
-export class APIPage extends Component  {
-  render() {
-    return (
-      <div className="start-working">
-        <InlineSVG className="hammer" src={ImageHammer}/>
-        <div className="docs">
-          Parse Server has extensive <a className="docs-link" href="http://parseplatform.github.io/docs/" target="_blank">Documentation</a>. Take a look!
-          <div className="hint">Our Interactive API Docs will be coming soon...</div>
-        </div>
+function APIPage() {
+  return (
+    <div className="start-working">
+      <InlineSVG className="hammer" src={ImageHammer}/>
+      <div className="docs">
+        Parse Server has extensive <a className="docs-link" href="http://parseplatform.github.io/docs/" target="_blank">Documentation</a>. Take a look!
+        <div className="hint">Our Interactive API Docs will be coming soon...</div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-export class APIPageContainer extends Component  {
-  render () {
-    const {models, nav} = this.props;
+function APIPageContainer({models, nav}) {
+  let title = `Chisel`;
+  let content = nav.initEnded ? <NoRights /> : null;
 
-    let title = `Chisel`;
-    let content = nav.initEnded ? <NoRights /> : null;
+  const curSite = models.currentSite;
+  if (curSite) {
+    title = `API - Site: ${curSite.name} - Chisel`;
 
-    const curSite = models.currentSite;
-    if (curSite) {
-      title = `API - Site: ${curSite.name} - Chisel`;
-
-      const role = models.role;
-      if (role != ROLE_EDITOR)
-        content = <APIPage />;
-    }
-
-    return <>
-      <Helmet>
-        <title>{title}</title>
-      </Helmet>
-      {content}
-    </>;
+    const role = models.role;
+    if (role != ROLE_EDITOR)
+      content = <APIPage />;
   }
+
+  return <>
+    <Helmet>
+      <title>{title}</title>
+    </Helmet>
+    {content}
+  </>;
 }
 
 function mapStateToProps(state) {
--- src/containers/MainArea/Models/ModelsListContainer.js
+++ src/containers/MainArea/Models/ModelsListContainer.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import {Helmet} from "react-helmet-async";
