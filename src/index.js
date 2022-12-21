d27acd785d32582c7b1b6617ea4e5d9104d43d7a
Nikita Golovin <golovin.nik@gmail.com>
2022-12-22 00:59:51 +0400

    Temp (possibly) fix for router working on Electron in Windows.

--- src/index.js
+++ src/index.js
@@ -7,13 +7,16 @@ import './styles.global.sss';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {HashRouter, BrowserRouter, Route, Routes} from "react-router-dom";
 import {HelmetProvider} from 'react-helmet-async';
 
 import App from 'containers/app';
 import configureStore from 'store/configureStore';
+import {isElectron} from 'utils/common';
 import {initApp} from 'utils/initialize';
 
+
+const Router = isElectron() ? HashRouter : BrowserRouter;
 
 export const store = configureStore();
 
